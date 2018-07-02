336c1a86790e473144e5cf47375b04d6c0f38598
Anton <anton@adc.sh>
2018-07-02 17:57:38 +0100

    example

--- example/example.js
+++ example/example.js
@@ -1,5 +1,18 @@
+import { HTTPContext } from 'https-context'
 import aqt from '../src'
 
 (async () => {
-  await aqt()
+  let c
+  try {
+    c = new HTTPContext()
+    await c._init()
+    c.setResponse('Hello World')
+    const res = await aqt(c.url)
+    const i = JSON.stringify(res, null, 2)
+    process.stdout.write(`${i}\n`)
+  } catch (err) {
+    process.stderr.write(`${err.message}\n`)
+  } finally {
+    await c._destroy()
+  }
 })()
