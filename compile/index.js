8069a21dc647b388e2ec5aa0fee9484f9bf59099
Anton <anton@adc.sh>
2019-07-29 19:27:49 +0300

    opts

--- compile/index.js
+++ compile/index.js
@@ -4,6 +4,14 @@ const _aqt = require('./aqt')
  * Request a web page and return information including `headers`, `statusCode`, `statusMessage` along with the `body` (which is also parsed if JSON received).
  * @param {string} address The URL such as http://example.com/api.
  * @param {_rqt.AqtOptions} [options] Configuration for requests.
+ * @param {!Object} [options.data] Optional data to send to the server with the request.
+ * @param {string} [options.type="json"] How to send data: `json` to serialise JSON data and add _Content-Type: application/json_ header, and `form` for url-encoded transmission with _Content-Type: application/x-www-form-urlencoded_. _Multipart/form-data_ must be implemented manually. Default `json`.
+ * @param {!http.OutgoingHttpHeaders} [options.headers] Headers to use for the request.
+ * @param {boolean} [options.compress=true] Add the `Accept-Encoding: gzip, deflate` header to indicate to the server that it can send a compressed response. Default `true`.
+ * @param {number} [options.timeout] The timeout after which the request should fail.
+ * @param {string} [options.method] What HTTP method to use in making of the request. When no method is given and `data` is present, defaults to `POST`.
+ * @param {boolean} [options.binary=false] Whether to return a buffer instead of a string. Default `false`.
+ * @param {boolean} [options.justHeaders=false] Whether to stop the request after response headers were received, without waiting for the data. Default `false`.
  */
 async function aqt(address, options) {
   const res = /** @type {_rqt.AqtReturn} */ (await _aqt(address, options))
